dbc449534b04b05e7f92eba91a8e65c2a6e7d2da
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2021-06-18 07:01:22 +1200

    add routes and edit wbpack to remove unecessary css

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+import Home from './Home'
 
 function App (props) {
-
   return (
     <>
-      <div className='app'>
+      <div className='title'>
         Xbox Friday Project App
+      </div>
+      <div className='main'>
+        <Route exact path='/' component={Home} />
       </div>
     </>
   )
---
+++ client/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+            The contents of the Home &quote/&quote should be displayed here
+    </>
+  )
+}
+
+export default Home
